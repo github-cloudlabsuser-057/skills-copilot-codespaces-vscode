a9d8a0116c02f4caf52d0aecdfff85b64f2f3f09
github-cloudlabsuser-057 <80259084+github-cloudlabsuser-057@users.noreply.github.com>
2024-07-08 12:17:53 +0000

    Copilot third commit

--- powershell.ps1
+++ powershell.ps1
@@ -1,13 +1,13 @@
-# Login to Azure
-Connect-AzAccount
-
-# Set variables
-$resourceGroupName = "myResourceGroup"
-$location = "westus"
-$storageAccountName = "mystorageaccount"
+# Define parameters
+$resourceGroupName = "example-resources"
+$location = "West Europe"
+$storageAccountName = "examplestorageacc"
 
 # Create a resource group
 New-AzResourceGroup -Name $resourceGroupName -Location $location
 
 # Create a storage account
-New-AzStorageAccount -ResourceGroupName $resourceGroupName -Name $storageAccountName -Location $location -SkuName Standard_LRS
+New-AzStorageAccount -ResourceGroupName $resourceGroupName `
+    -Name $storageAccountName `
+    -Location $location `
+    -SkuName "Standard_GRS"
